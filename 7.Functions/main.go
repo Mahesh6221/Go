4322b5348ba7ca9837db156eae10ed4122eb1820
Mahesh6221 <maheshpatil6221@gmail.com>
2024-06-08 11:19:42 +0530

    constant

--- 7.Functions/main.go
+++ 7.Functions/main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("welcome to function in golang")
 	fmt.Println(add(20, 30))
+	simplefunction()
+}
+
+func simplefunction() {
+	fmt.Println("hello from simple function")
 }
 
 func add(x, y int) int {
