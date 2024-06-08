6044f95cb8515a34229cf20a4059bf6d6310501e
Mahesh6221 <maheshpatil6221@gmail.com>
2024-06-08 12:14:06 +0530

    constant

--- 7.Functions/main.go
+++ 7.Functions/main.go
@@ -2,16 +2,40 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("welcome to function in golang")
-	fmt.Println(add(20, 30))
-	simplefunction()
-}
-
 func simplefunction() {
 	fmt.Println("hello from simple function")
 }
 
+//function with parameters and return type
 func add(x, y int) int {
+	//input parameters are x and y and output parameter
+	//is int because the output in the
+	//integer format.
 	return x + y
+}
+
+//function with named return variable
+func multiply(x, y int) (result int) {
+	//i already said that the variable is result which gets returned
+	//the result is written for the output
+	// result is the return type we return the result variable
+	result = x * y //here we are performing some operation
+	return         //here the returned value should be a result having datatype int
+}
+
+func subtract(x, y int) (result int) {
+	result = x - y
+	return
+}
+
+func main() {
+	fmt.Println("welcome to function in golang")
+	fmt.Println(add(20, 30))
+	simplefunction()
+	ans := add(3, 4)
+	fmt.Println(ans)
+	product := multiply(4, 6)
+	fmt.Println(product)
+	minus := subtract(5, 3)
+	fmt.Println(minus)
 }
