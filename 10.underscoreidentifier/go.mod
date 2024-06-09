feeac0418889e89c26841c34c052591893055f3b
Mahesh6221 <maheshpatil6221@gmail.com>
2024-06-09 13:58:51 +0530

    10.underscoreidentifier

---
+++ 10.underscoreidentifier/go.mod
@@ -0,0 +1,3 @@
+module 10.underscoreidentifier
+
+go 1.22.4
---
+++ 10.underscoreidentifier/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func divide(a, b float64) (float64, error) { //error is the standard format which here i write in this line which return error
+	if b == 0 {
+		return 0, fmt.Errorf("denominator must not be zero")
+	}
+	return a / b, nil
+}
+
+// func main() {
+// 	//In error handling we normally handle the errors as we do in other programming,but
+// 	//in go we use _ underscore to handle the error it is called as blank identifier.
+// 	fmt.Println("error handling in golang")
+// 	div, _ := divide(10, 5)
+// 	fmt.Println("division of two numbers", div)
+// }
+
+func main() {
+	fmt.Println("error handling in golang")
+	div, err := divide(10, 4)
+	if err != nil {
+		fmt.Println("error handling")
+	}
+	fmt.Println("division of two numbers", div)
+}
